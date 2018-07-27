7fb358ddcfc5d6e2fa5d142d29c5039f4c38d7a5
rubycouzcedric <cousin.ruby.cedric@gmail.com>
2018-07-27 11:13:16 +0200

    exo2 et 4

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <title>Exercice 1</title>
   </head>
   <body>
-    <form class="" action="user.php" method="post">
+    <form class="" action="user.php" method="POST">
       <input type="text" name="lastname" placeholder="Nom" />
       <input type="text" name="firstname" placeholder="Prénom">
       <input type="submit" value="Valider">
